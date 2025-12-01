4d38aa05bfa9a0a6e5054b3f0e6af5546c4002af
quiquee06 <enrique.vara.2006@gmail.com>
2025-12-01 21:13:25 +0100

    Empresa

---
+++ src/main/java/Company.java
@@ -0,0 +1,59 @@
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Company {
+    private String name;
+    private String cif;
+    private Department[] departments;
+
+    public Company(String name, String cif, Department[] departments) {
+        this.name = name;
+        this.cif = cif;
+        this.departments = departments;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCif() {
+        return cif;
+    }
+
+    public void setCif(String cif) {
+        this.cif = cif;
+    }
+
+    public Department[] getDepartments() {
+        return departments;
+    }
+
+    public void setDepartments(Department[] departments) {
+        this.departments = departments;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Company company = (Company) o;
+        return Objects.equals(name, company.name) && Objects.equals(cif, company.cif) && Objects.deepEquals(departments, company.departments);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, cif, Arrays.hashCode(departments));
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "name='" + name + '\'' +
+                ", cif='" + cif + '\'' +
+                ", departments=" + Arrays.toString(departments) +
+                '}';
+    }
+}
