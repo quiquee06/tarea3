4f2a008c2f637bb276593ee940e9f5309fa3b330
quiquee06 <enrique.vara.2006@gmail.com>
2025-12-06 03:03:27 +0100

    Reader de la clase Empleado

---
+++ src/main/java/readers/EmployeeReader.java
@@ -0,0 +1,35 @@
+package readers;
+
+import model.Employee;
+
+import java.util.Scanner;
+
+public class EmployeeReader {
+    private final Scanner sc;
+
+    public EmployeeReader(Scanner sc) {
+        this.sc = sc;
+    }
+
+    public Employee read() {
+        System.out.println("Introduce los datos del empleado");
+        System.out.println("NIF:");
+        String nif = sc.nextLine();
+
+        System.out.println("Nombre:");
+        String name = sc.nextLine();
+
+        System.out.println("Apellidos:");
+        String surname = sc.nextLine();
+
+        System.out.println("Puesto:");
+        String position = sc.nextLine();
+
+        return new Employee(
+                nif,
+                name,
+                surname,
+                position
+        );
+    }
+}
